a95a3636accdd84f7f121351db96c4a8a568c0ae
Naomi Cherotich <naomirono1454@gmail.com>
2023-04-17 20:39:10 +0300

    Add Splash Screen component to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,36 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import SplashScreen from './components/SplashScreen'
 import './App.css'
 
 function App() {
-  
+  const [storedToken, setStoredToken] = useState(localStorage.getItem("token"));
+  const [loggedInUserId, setLoggedInUserId] = useState("");
+  useEffect(() => {
+    fetch("http://127.0.0.1:3000/api/v1/profile ", {
+      method: "GET",
+      headers: {
+        Accepts: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setLoggedInUserId(data.user.id);
+      });
+  }, [storedToken]);
 
   return (
+    <div className="App bg-black">
     
+    <Routes>
+      
+    {storedToken ? null : 
+    <Route path="/" element={<SplashScreen />} />}
+
+    </Routes>
+  </div>
   )
 }
 
