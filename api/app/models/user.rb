614f27724e8ba18aeb569b1b4a763797f42590e5
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-04-17 07:29:06 +0300

    Added validations

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
     validates :email, uniqueness: true
+    validates :email, presence: true
+    validates :password, presence: true
+    validates :username, presence: true
+
 
 end
