bf702862eec90f25b50519d408a89a73afc8c6ab
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-04-22 16:32:24 +0300

    Added create action for reviews

--- api/app/controllers/reviews_controller.rb
+++ api/app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
+
+    def create
+        review = Review.create(review_params)
+        render json: review
+    end
+
+
+    private
+
+    def review_params
+        params.require(:review).permit(:content, :score, :product_id)
+    end
+    
+
 end
