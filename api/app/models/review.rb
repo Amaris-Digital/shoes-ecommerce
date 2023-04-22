53977ea21af5d01fafce2c3590a4c8e2d6091619
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-04-22 16:31:34 +0300

    Added model for review and serializer

---
+++ api/app/models/review.rb
@@ -0,0 +1,4 @@
+class Review < ApplicationRecord
+  belongs_to :product
+  validates :content, :score, presence: true
+end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :reviews
   resources :products
   # config/routes.rb
   namespace :api do
